49a772e3099b6eb9821d0ac1ebf9319025ad98fd
jacktrusler <jtrus93@gmail.com>
2026-02-28 20:28:07 -0500

    add location cards 🏖️

--- web/gameState.go
+++ web/gameState.go
@@ -2,31 +2,38 @@ package web
 
 import (
 	"context"
+	"crabspy"
 	"crabspy/sql/sqlcgen"
 	"encoding/json"
 	"time"
 )
 
 type GameState struct {
-	RoomID        int64
-	SpyID         int64
-	Location      string
-	StartedAt     int64
-	TimerDuration int64
-	Paused        bool
-	PausedID      int64 // 0 = nobody
-	AccusedID     int64 // 0 = nobody
-	Events        []sqlcgen.GameEvent
+	RoomID          int64
+	SpyID           int64
+	Location        crabspy.Location
+	StartedAt       int64
+	TimerDuration   int64
+	Paused          bool
+	PausedID        int64 // 0 = nobody
+	AccusedID       int64 // 0 = nobody
+	Events          []sqlcgen.GameEvent
 	HasPaused       map[int64]bool
 	HasAccused      map[int64]bool
 	GuessedLocation string
 }
 
 func BuildGameState(game sqlcgen.Game, events []sqlcgen.GameEvent) GameState {
+	var location crabspy.Location
+	for _, l := range crabspy.Locations {
+		if l.Title == game.Location {
+			location = l
+		}
+	}
 	state := GameState{
 		RoomID:        game.RoomID,
 		SpyID:         game.SpyID,
-		Location:      game.Location,
+		Location:      location,
 		StartedAt:     game.StartedAt,
 		TimerDuration: game.TimerDuration,
 		Events:        events,
